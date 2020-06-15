b54ee34d5e21b70ccf9787332a7d3a039384fd23
Eneko Bikandi <ebikandi001@gmail.com>
2020-06-15 23:55:34 +0200

    [Tarea21]: fix floating point issue

--- tarea_21/app.py
+++ tarea_21/app.py
@@ -1,30 +1,33 @@
 def get_gcd(num, denom):
-  div = num
-  while div in range(num + 1):
-    if num % div == 0 and denom % div == 0:
-      return div
-    div -= 1
+    div = num
+    while div in range(num + 1):
+        if num % div == 0 and denom % div == 0:
+            return div
+        div -= 1
+
 
 def minimum_fraction(num):
-  if 0 < num < 1:
-    denom = 10000
-    multiplied = int(num * denom)
-    # MCD se puede calcular usando Math.gcd() pero para el ejercicio lo calcularemos a mano.
-    divisor = get_gcd(multiplied, denom)
-    min_num = multiplied/divisor
-    min_denom = denom/divisor
-    print("Fraccion minima de %.4f: %d/%d" %(num, min_num, min_denom))
-  else:
-    raise Exception()
+    if 0 < num < 1:
+        denom = 10000
+        # Redondear para evitar el error por punto flotante.
+        multiplied = int(round(num * denom))
+        # MCD se puede calcular usando Math.gcd() pero para el ejercicio lo calcularemos a mano.
+        divisor = get_gcd(multiplied, denom)
+        min_num = multiplied/divisor
+        min_denom = denom/divisor
+        print("Fraccion minima de %.4f: %d/%d" % (num, min_num, min_denom))
+    else:
+        raise Exception()
+
 
 while True:
-  try:
-    number = float(input("Mete un número entre [0.0001, 0.9999]: (1 para salir)\n"))
-    if int(number) == 1:
-      break
-    else:
-      minimum_fraction(number)
-  except:
-    print("Mal input")
-    continue
-  
+    try:
+        number = float(
+            input("Mete un número entre [0.0001, 0.9999]: (1 para salir)\n"))
+        if int(number) == 1:
+            break
+        else:
+            minimum_fraction(number)
+    except:
+        print("Mal input")
+        continue
