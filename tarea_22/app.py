fe32456edae50384b35b03d720ad16444ae5a9ea
Eneko Bikandi <eneko.bikandi@ve.com>
2020-06-16 11:02:17 +0200

    [Tarea22]: recursive solution

--- tarea_22/app.py
+++ tarea_22/app.py
@@ -1,43 +1,56 @@
-# from random as r  # Random number generators
 from cow import Cow  # Custom class
 
-# seed random number generator
+def max(x, y):
+    if(x > y):
+        return x
+    return y
+
+def getMaxProduction(cowsInMarket, maxWeight, index, actualMilk, actualWeight):
+    if(index < len(cowsInMarket)):
+        actualCow = cowsInMarket[index]
+        if(actualWeight + actualCow.weight > maxWeight):
+            return getMaxProduction(cowsInMarket, maxWeight, index +1 , actualMilk, actualWeight)
+        else:
+            return max(getMaxProduction(cowsInMarket, maxWeight, index +1, actualMilk + actualCow.milk, actualWeight + actualCow.weight),
+                getMaxProduction(cowsInMarket, maxWeight, index +1, actualMilk, actualWeight))
+    return actualMilk
+
 
 if __name__ == '__main__':
-    cowsInMarket = []
+    cowsInMarket = [
+        Cow(1, 360, 40),
+        Cow(2, 250, 35),
+        Cow(3, 400, 43),
+        Cow(4, 180, 28),
+        Cow(5, 50, 12),
+        Cow(6, 90, 13),
+    ]
+    # cowsInMarket = []
     boughtCows = []
     weightToCarry = 0
     milkProduction = 0
-    totalCows = int(input("¿Cuantas vacas hay en Tolosa?\n"))
-    maxWeight = int(input("¿Cuanto peso puedes transportar?\n"))
-    for i in range(1, totalCows+1):
-        weight = int(input("Introducir peso vaca %d:\n" % i))
-        milk = int(input("Introducir cantidad de leche de la vaca %d:\n" % i))
-        newCow = Cow(i, weight, milk)
-        cowsInMarket.append(newCow)
+    # totalCows = int(input("¿Cuantas vacas hay en Tolosa?\n"))
+    maxWeight = 700 #int(input("¿Cuanto peso puedes transportar?\n"))
+    # for i in range(1, totalCows+1):
+    #     weight = int(input("Introducir peso vaca %d:\n" % i))
+    #     milk = int(input("Introducir cantidad de leche de la vaca %d:\n" % i))
+    #     newCow = Cow(i, weight, milk)
+    #     cowsInMarket.append(newCow)
 
-    # ordenar las vacas por el ratio en orden descendente
-    cowsInMarket.sort(key=lambda c: c.ratio, reverse=True)
+    
+    
     # ir cogiendo las vacas y calculando la produccion de leche hasta llegar al peso maximo
-    for c in cowsInMarket:
-        remainingWeight = maxWeight - weightToCarry
-        if c.weight > remainingWeight:
-            continue
-        else:
-            boughtCows.append(c)
-            milkProduction += c.milk
-            weightToCarry += c.weight
-            if weightToCarry == maxWeight:
-                break
+    maxProduction = getMaxProduction(cowsInMarket, maxWeight, 0, 0, 0)
+    print("MAX: %d" % maxProduction )    
 
     # Imprimir resultado
-    print("\n******************************************\n")
-    print("Produccion maxima de leche: %d" % milkProduction)
-    print("Peso a llevar: %d; Peso maximo estipulado: %d" %
-          (weightToCarry, maxWeight))
-    print("Vacas compradas:")
-    for c in boughtCows:
-        c.printInfo()
-    print("Todas las vacas:")
-    for c in cowsInMarket:
-        c.printInfo()
+    # print("\n******************************************\n")
+    # print("Produccion maxima de leche: %d" % milkProduction)
+    # print("Peso a llevar: %d; Peso maximo estipulado: %d" %
+    #       (weightToCarry, maxWeight))
+    # print("Vacas compradas:")
+    # for c in boughtCows:
+    #     c.printInfo()
+    # print("Todas las vacas:")
+    # for c in cowsInMarket:
+    #     c.printInfo()
--- tarea_22/readme.md
+++ tarea_22/readme.md
@@ -3,3 +3,26 @@
 # Algoritmo del lechero
 
 [Link al diccionario](https://www.notion.so/5f0d36e27bb448149f07eca7ee5c2b49?v=3123ca19b70f451eb6e33877298a446f)
+
+
+## Datos de prueba
+```
+Vaca / Peso en kilogramos / Producción de leche por día
+1 360 40
+2 250 35
+3 400 43
+4 180 28
+5 50 12
+6 90 13
+
+Camión de 700 kg
+
+Vacas escogidas:
+
+Vaca / Peso en kilogramos / Producción de leche por día
+1 360 40
+4 180 28
+5 50 12
+6 90 13
+4x, total de 700kg,  producción total de leche 93L
+```
